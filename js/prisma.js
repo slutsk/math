63e2e79ec9bb9fadbd0b9286b52c61ac7a3ebd45
slutsk <54749775+slutsk@users.noreply.github.com>
2022-07-30 20:34:46 +0300

    Add files via upload

--- js/prisma.js
+++ js/prisma.js
@@ -10,25 +10,26 @@ function drawLine( ctx ,x1, y1, x2, y2, color, lineWidth, lineDash){
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
+    
 }
 
 /**
  * четырёхугольник
  */
 
-function quadrilateral(cxt, x1, y1, x2, y2, x3, y3, x4, y4, width ,fillColor, strokeColor, fill){
+function quadrilateral(cxt, x1, y1, x2, y2, x3, y3, x4, y4, width ,fillColor, strokeColor, fill_){
+    cxt.beginPath();
     cxt.fillStyle = fillColor;
     cxt.strokeStyle = strokeColor;
     cxt.lineWidth = width;
-    cxt.beginPath();
-  
+      
     cxt.moveTo(x1, y1);
     cxt.lineTo(x2, y2); 
     cxt.lineTo(x3, y3);
     cxt.lineTo(x4, y4);
     cxt.lineTo(x1, y1);
 
-    if(fill) cxt.fill();
+    if(fill_) cxt.fill();
     
     cxt.stroke();
 }
@@ -76,47 +77,21 @@ ctx01.fillStyle = "white";
 ctx01.fillText("основание", x - 10, y + 50);
 ctx01.fillText("основание", x - 90, y + 220);
 
+//----------------------------------------------------------------------------
+
+//БОКОВЫЕ ГРАНИ           БОКОВЫЕ ГРАНИ
 
 x = 820;
 k = 20;
-ctx01.beginPath();
-ctx01.fillStyle = "red";
-//back
-drawLine(ctx01, x, y, x + a, y, "lightblue", 3, 0);
-drawLine(ctx01, x + left, y + h, x + a + left, y + h, "lightblue", 3, 10);
-drawLine(ctx01, x + left, y + h, x, y, "lightblue", 3, 10);
-drawLine(ctx01, x + left + 40, y + h - 80, x, y, "lightblue", 3, 0);
-drawLine(ctx01, x + left + k, y + h - 80, x - k, y, "lightblue", 3, 0);
-drawLine(ctx01, x + a + left, y + h, x + a, y, "lightblue", 3, 10);
-                   //drawLine(ctx01, x + a + left, y + h, x + a, y, "lightblue", 3, 0);
-ctx01.fill();
+ctx01.setLineDash([0]);
 
-drawLine(ctx01, x + a + k, y, x + a2 + k, y + b, "lightblue", 3, 0);
-drawLine(ctx01, x + a2, y + b, x - a2, y + b, "lightblue", 3, 0);
-drawLine(ctx01, x - k, y , x - a2 - k, y + b, "lightblue", 3, 0);
-
-drawLine(ctx01, x + a + left + k, y + h, x + a2 + left + k, y + b + h, "lightblue", 3, 0);
-drawLine(ctx01, x + a2 + left, y + b + h, x - a2 + left, y + b + h, "lightblue", 3, 0);
-drawLine(ctx01, x + left - k, y + h, x - a2 + left - k, y + b + h, "lightblue", 3, 10);
-drawLine(ctx01, x + left - k, y + h, x - a2 + left - k, y + b + h, "lightblue", 3, 10);
-drawLine(ctx01, x + left - k - 70, y + h + 70, x - a2 + left - k, y + b + h, "lightblue", 3, 0);
-
-
-drawLine(ctx01, x + left - k, y + h, x - k, y, "lightblue", 3, 10);
-
-drawLine(ctx01, x + a + left+k, y + h, x + a+k, y, "lightblue", 3, 0);
-drawLine(ctx01, x + a2 + left, y + b + h, x + a2, y + b, "lightblue", 3, 0);
-drawLine(ctx01, x + a2 + left + k, y + b + h, x + a2 + k, y + b, "lightblue", 3, 0);
-drawLine(ctx01, x - a2 + left, y + h + b, x - a2, y + b, "lightblue", 3, 0);
-drawLine(ctx01, x - a2 + left - k, y + h + b, x - a2 - k, y + b, "lightblue", 3, 0);
-
-const canvas02 = d.getElementById("prism02");
-const ctx02 = canvas02.getContext("2d");
 xc += 620;
 let dx = -100;
 let dy = 100;
-quadrilateral(ctx01, xc, yc, xc + a, yc, xc + a + left , yc + h, xc + left  , yc + h, 3, "lightred", "lightblue", true);
+quadrilateral(ctx01, xc, yc, xc + a, yc, xc + a + left , yc + h, xc + left  , yc + h, 3, "red", "lightblue", true);
 quadrilateral(ctx01, xc - k, yc, xc - a2 - k, yc + b, xc - a2 - k + left, yc + h + b, xc - k + left, yc + h, 3, "green", "lightblue", true);
 quadrilateral(ctx01, xc + k + a, yc, xc - a2 + k + a, yc + b, xc - a2 + left + k + a, yc + h + b, xc + left + k + a, yc + h, 3, "blue", "lightblue", true);
 quadrilateral(ctx01, xc + dx, yc + dy, xc + a + dx, yc + dy, xc + a + left + dx , yc + h + dy, xc + left + dx , yc + h + dy, 3, "orange", "lightblue", true);
 
+const canvas02 = d.getElementById("prism02");
+const ctx02 = canvas02.getContext("2d");
